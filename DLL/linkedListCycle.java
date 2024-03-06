eafbb97bd323159ee6d341c855019e909e3fc006
Keerthi <keeerthireddy333@gmail.com>
2024-03-06 21:50:22 +0530

    Check cycle in Linekd List

---
+++ DLL/linkedListCycle.java
@@ -0,0 +1,26 @@
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class linkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+        ListNode fast = head, slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) 
+                return true;
+        }
+        return false;
+    }
+
+    
+}
